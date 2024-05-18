cdbd4433a711d978e07b378c0c85c5143c6009a4
Dhruvwalia <99418298+waliadhruv1010@users.noreply.github.com>
2024-05-18 23:41:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var istatus = document.querySelector('h5');
+var btn =document.querySelector('#add');
+
+var check =0;
+btn.addEventListener("click",function(){
+    if(check == 0){
+    istatus.innerHTML='Friend';
+    istatus.style.color='green';
+    btn.innerHTML='Remove Friend';
+    btn.style.backgroundColor='grey';
+    check=1;
+    }else{
+    istatus.innerHTML='Stranger';
+    istatus.style.color='red';
+    btn.innerHTML='Add Friend';
+    btn.style.backgroundColor='green';
+    check=0;
+    }
+    
+})
+
